0b4f8a32e95271440939b347b4ec9eb017c66b76
Alan pascke <alan.pascke@gmail.com>
2023-06-29 11:10:14 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,28 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens:{
+      'cell': '350px',
+      // => @media (min-width: 475px) { ... }
+      
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'xm': '850px',
+      // => @media (min-width: 850px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
